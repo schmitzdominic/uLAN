d252291300635181ece03c244764d50655626202
Dominic Schmitz <schmitzdominic@googlemail.com>
2020-08-12 18:52:57 +0200

    Now the Download path can be set inside the settings

--- src/pages/SettingsController.java
+++ src/pages/SettingsController.java
@@ -5,8 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+import registry.Registry;
 import start.MainController;
 
+import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
@@ -21,20 +24,29 @@ public class SettingsController implements Initializable {
     @FXML
     Label labelDownloadPath;
 
-    HashMap<String, String> settings;
+    HashMap<String, String> settings = getSettings();
+    Registry registry = new Registry();
 
     final String descriptionDownloadPath = "Hier werden alle deine Downloads gespeichert";
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
-        this.settings = getSettings();
-
         this.initData();
     }
 
     public void initData() {
         this.labelTextDownloadPath.setText(this.descriptionDownloadPath);
         this.labelDownloadPath.setText(this.settings.get("defaultfiletransferpath"));
+    }
+
+    public void buttonChooseDownloadPath(final ActionEvent event) {
+        final DirectoryChooser directoryChooser = new DirectoryChooser();
+        final File selectedDirectory = directoryChooser.showDialog(MainController.settingsStage);
+        if (selectedDirectory != null) {
+            final String path = selectedDirectory.getAbsolutePath();
+            this.registry.setSetting("properties", "defaultfiletransferpath", path);
+            this.labelDownloadPath.setText(path);
+        }
     }
 
     public void buttonOk(final ActionEvent event) {
