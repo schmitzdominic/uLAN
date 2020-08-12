c635f02a4ebb9bcd0b5a99306055199188b3dc7a
Dominic Schmitz <schmitzdominic@googlemail.com>
2020-08-12 18:40:02 +0200

    Style changed to be more Windows settings like

--- src/pages/SettingsController.java
+++ src/pages/SettingsController.java
@@ -2,7 +2,9 @@ package pages;
 
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import start.MainController;
 
 import java.net.URL;
@@ -13,15 +15,26 @@ import static info.Info.getSettings;
 
 public class SettingsController implements Initializable {
 
+    @FXML
+    Label labelTextDownloadPath;
+
+    @FXML
+    Label labelDownloadPath;
+
     HashMap<String, String> settings;
+
+    final String descriptionDownloadPath = "Hier werden alle deine Downloads gespeichert";
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
         this.settings = getSettings();
+
+        this.initData();
     }
 
     public void initData() {
-
+        this.labelTextDownloadPath.setText(this.descriptionDownloadPath);
+        this.labelDownloadPath.setText(this.settings.get("defaultfiletransferpath"));
     }
 
     public void buttonOk(final ActionEvent event) {
