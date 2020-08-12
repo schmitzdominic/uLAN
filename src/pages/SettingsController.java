189d5fe9243bed7317f34f15851e0bbd98738d89
Dominic Schmitz <schmitzdominic@googlemail.com>
2020-08-12 17:38:26 +0200

    Settings skeleton created

--- src/pages/SettingsController.java
+++ src/pages/SettingsController.java
@@ -1,17 +1,30 @@
 package pages;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import start.MainController;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
+
+import static info.Info.getSettings;
 
 public class SettingsController implements Initializable {
 
+    HashMap<String, String> settings;
+
     @Override
-    public void initialize(URL location, ResourceBundle resources) {}
+    public void initialize(final URL location, final ResourceBundle resources) {
+        this.settings = getSettings();
+    }
 
     public void initData() {
 
+    }
+
+    public void buttonOk(final ActionEvent event) {
+        MainController.settingsStage.close();
     }
 }
